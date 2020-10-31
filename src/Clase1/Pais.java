ce48a6744b749331a6f4feb1e407fa207a9dad7d
Pablo <pablo@Arch.localhost>
2020-10-30 22:16:52 -0600

    Problema Pais

---
+++ src/Clase1/Pais.java
@@ -0,0 +1,12 @@
+package Clase1;
+
+/**
+ * Pais
+ */
+public class Pais {
+
+    public static void main(String[] args) {
+        int n = 9;
+        System.out.println((n == 1) ? 0 : n * 25);
+    }
+}
